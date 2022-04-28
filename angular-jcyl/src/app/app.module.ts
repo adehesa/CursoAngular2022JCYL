3dce11fc3da375292bdaa55e3f2363c58d7615bd
Angel Dehesa Sanchez <angel.dehesa@jcyl.es>
2022-04-28 08:10:05 +0200

    Añadir componente solicitud y cambios para Ej. 10

--- angular-jcyl/src/app/app.module.ts
+++ angular-jcyl/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CentroComponent } from './centro/centro.component';
+import { SolicitudComponent } from './solicitud/solicitud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CentroComponent
+    CentroComponent,
+    SolicitudComponent
   ],
   imports: [
     BrowserModule,
--- angular-jcyl/src/app/centro/centro.component.ts
+++ angular-jcyl/src/app/centro/centro.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-centro',
-  //templateUrl: './centro.component.html',
-  template: `
-    <div>
-      {{centro.txt}}
-    <div>`,
+  templateUrl: './centro.component.html',
   styleUrls: ['./centro.component.css']
 })
 export class CentroComponent implements OnInit {
 
-  centro = {txt: 'Prueba template embebido'}
+  @Input() centro: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
