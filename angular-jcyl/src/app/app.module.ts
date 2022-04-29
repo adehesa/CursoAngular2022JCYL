7a38f6cf634c4d2cf47d2257af615fe490f89c99
Angel Dehesa Sanchez <angel.dehesa@jcyl.es>
2022-04-29 13:28:46 +0200

    Parto de un ejemplo vacío

--- angular-jcyl/src/app/app.module.ts
+++ angular-jcyl/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CentroComponent } from './centro/centro.component';
-import { SelectorCentroComponent } from './selector-centro/selector-centro.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CentroComponent,
-    SelectorCentroComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
