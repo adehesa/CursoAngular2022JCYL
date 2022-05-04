75550d4bc3889312958ee6635c769f41f5660ea5
Angel Dehesa Sanchez <angel.dehesa@jcyl.es>
2022-05-04 12:57:16 +0200

    Usar tanto Router como ActivatedRoute

--- angular-jcyl/src/app/app.component.ts
+++ angular-jcyl/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-jcyl';
+
+  constructor(private route: Router) {
+
+  }
+
+  irToCentro() {
+    this.route.navigate(['/centro', '1'])
+  }
+
 }
+
